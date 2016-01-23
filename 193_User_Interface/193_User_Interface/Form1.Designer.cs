65eb9c55ed1ae630901e1da34dec9be0da66b767
Birgit Fleming <birgit.fleming@gmail.com>
2016-01-22 19:55:50 -0800

    Able to read in "dummy" serial data.

--- 193_User_Interface/193_User_Interface/Form1.Designer.cs
+++ 193_User_Interface/193_User_Interface/Form1.Designer.cs
@@ -30,15 +30,24 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ManiXButton.Office2010White office2010White1 = new ManiXButton.Office2010White();
             ManiXButton.Office2010Red office2010Red1 = new ManiXButton.Office2010Red();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.gmap = new GMap.NET.WindowsForms.GMapControl();
             this.ToolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.xButton5 = new ManiXButton.XButton();
+            this.xButton4 = new ManiXButton.XButton();
+            this.xButton3 = new ManiXButton.XButton();
+            this.xButton2 = new ManiXButton.XButton();
             this.xButton1 = new ManiXButton.XButton();
+            this.xButton6 = new ManiXButton.XButton();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -89,7 +98,15 @@
             // 
             // splitContainer2.Panel1
             // 
-            this.splitContainer2.Panel1.Controls.Add(this.richTextBox1);
+            this.splitContainer2.Panel1.Controls.Add(this.xButton6);
+            this.splitContainer2.Panel1.Controls.Add(this.xButton5);
+            this.splitContainer2.Panel1.Controls.Add(this.xButton4);
+            this.splitContainer2.Panel1.Controls.Add(this.xButton3);
+            this.splitContainer2.Panel1.Controls.Add(this.xButton2);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox4);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox3);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
             this.splitContainer2.Panel1.Controls.Add(this.dateTimePicker1);
             this.splitContainer2.Panel1.Controls.Add(this.xButton1);
             // 
@@ -99,6 +116,42 @@
             this.splitContainer2.Size = new System.Drawing.Size(1364, 657);
             this.splitContainer2.SplitterDistance = 547;
             this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(399, 247);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(135, 22);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(270, 247);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(123, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 247);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // dateTimePicker1
             // 
@@ -136,19 +189,92 @@
             this.gmap.Zoom = 10D;
             this.gmap.Load += new System.EventHandler(this.gmap_Load_1);
             // 
-            // richTextBox1
+            // xButton5
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(164, 157);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(248, 247);
-            this.richTextBox1.TabIndex = 4;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            office2010White1.BorderColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
+            office2010White1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
+            office2010White1.ButtonMouseOverColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010White1.ButtonMouseOverColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010White1.ButtonMouseOverColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(137)))));
+            office2010White1.ButtonMouseOverColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(249)))), ((int)(((byte)(224)))));
+            office2010White1.ButtonNormalColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(154)))), ((int)(((byte)(154)))));
+            office2010White1.ButtonNormalColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            office2010White1.ButtonNormalColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            office2010White1.ButtonNormalColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            office2010White1.ButtonSelectedColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010White1.ButtonSelectedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010White1.ButtonSelectedColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(117)))));
+            office2010White1.ButtonSelectedColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(216)))), ((int)(((byte)(107)))));
+            office2010White1.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010White1.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010White1.TextColor = System.Drawing.Color.Black;
+            this.xButton5.ColorTable = office2010White1;
+            this.xButton5.Font = new System.Drawing.Font("Arial Black", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xButton5.Location = new System.Drawing.Point(399, 203);
+            this.xButton5.Name = "xButton5";
+            this.xButton5.Size = new System.Drawing.Size(135, 38);
+            this.xButton5.TabIndex = 14;
+            this.xButton5.Text = "Date/ Time";
+            this.xButton5.Theme = ManiXButton.Theme.MSOffice2010_WHITE;
+            this.xButton5.UseVisualStyleBackColor = true;
+            this.xButton5.Click += new System.EventHandler(this.xButton5_Click);
+            // 
+            // xButton4
+            // 
+            this.xButton4.ColorTable = office2010White1;
+            this.xButton4.Font = new System.Drawing.Font("Arial Black", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xButton4.Location = new System.Drawing.Point(270, 203);
+            this.xButton4.Name = "xButton4";
+            this.xButton4.Size = new System.Drawing.Size(123, 38);
+            this.xButton4.TabIndex = 13;
+            this.xButton4.Text = "Heart Rate";
+            this.xButton4.Theme = ManiXButton.Theme.MSOffice2010_WHITE;
+            this.xButton4.UseVisualStyleBackColor = true;
+            this.xButton4.Click += new System.EventHandler(this.xButton4_Click);
+            // 
+            // xButton3
+            // 
+            this.xButton3.ColorTable = office2010White1;
+            this.xButton3.Font = new System.Drawing.Font("Arial Black", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xButton3.Location = new System.Drawing.Point(141, 203);
+            this.xButton3.Name = "xButton3";
+            this.xButton3.Size = new System.Drawing.Size(123, 38);
+            this.xButton3.TabIndex = 12;
+            this.xButton3.Text = "Longitude";
+            this.xButton3.Theme = ManiXButton.Theme.MSOffice2010_WHITE;
+            this.xButton3.UseVisualStyleBackColor = true;
+            this.xButton3.Click += new System.EventHandler(this.xButton3_Click);
+            // 
+            // xButton2
+            // 
+            this.xButton2.ColorTable = office2010White1;
+            this.xButton2.Font = new System.Drawing.Font("Arial Black", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xButton2.Location = new System.Drawing.Point(12, 203);
+            this.xButton2.Name = "xButton2";
+            this.xButton2.Size = new System.Drawing.Size(123, 38);
+            this.xButton2.TabIndex = 11;
+            this.xButton2.Text = "Latitude";
+            this.xButton2.Theme = ManiXButton.Theme.MSOffice2010_WHITE;
+            this.xButton2.UseVisualStyleBackColor = true;
+            this.xButton2.Click += new System.EventHandler(this.xButton2_Click);
             // 
             // xButton1
             // 
             this.xButton1.AutoSize = true;
+            this.xButton1.ColorTable = office2010Red1;
+            this.xButton1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xButton1.Location = new System.Drawing.Point(141, 70);
+            this.xButton1.Name = "xButton1";
+            this.xButton1.Size = new System.Drawing.Size(248, 38);
+            this.xButton1.TabIndex = 0;
+            this.xButton1.Text = "Click to Connect";
+            this.xButton1.Theme = ManiXButton.Theme.MSOffice2010_RED;
+            this.xButton1.UseVisualStyleBackColor = true;
+            this.xButton1.Click += new System.EventHandler(this.xButton1_Click_1);
+            // 
+            // xButton6
+            // 
+            this.xButton6.AutoSize = true;
             office2010Red1.BorderColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
             office2010Red1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
             office2010Red1.ButtonMouseOverColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(148)))), ((int)(((byte)(64)))));
@@ -166,16 +292,16 @@
             office2010Red1.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
             office2010Red1.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
             office2010Red1.TextColor = System.Drawing.Color.Black;
-            this.xButton1.ColorTable = office2010Red1;
-            this.xButton1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.xButton1.Location = new System.Drawing.Point(164, 75);
-            this.xButton1.Name = "xButton1";
-            this.xButton1.Size = new System.Drawing.Size(248, 38);
-            this.xButton1.TabIndex = 0;
-            this.xButton1.Text = "Click to Connect";
-            this.xButton1.Theme = ManiXButton.Theme.MSOffice2010_RED;
-            this.xButton1.UseVisualStyleBackColor = true;
-            this.xButton1.Click += new System.EventHandler(this.xButton1_Click_1);
+            this.xButton6.ColorTable = office2010Red1;
+            this.xButton6.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xButton6.Location = new System.Drawing.Point(141, 114);
+            this.xButton6.Name = "xButton6";
+            this.xButton6.Size = new System.Drawing.Size(248, 38);
+            this.xButton6.TabIndex = 15;
+            this.xButton6.Text = "Disconnect";
+            this.xButton6.Theme = ManiXButton.Theme.MSOffice2010_RED;
+            this.xButton6.UseVisualStyleBackColor = true;
+            this.xButton6.Click += new System.EventHandler(this.xButton6_Click);
             // 
             // Form1
             // 
@@ -209,7 +335,15 @@
         private ManiXButton.XButton xButton1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.ToolTip ToolTip1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private ManiXButton.XButton xButton2;
+        private ManiXButton.XButton xButton5;
+        private ManiXButton.XButton xButton4;
+        private ManiXButton.XButton xButton3;
+        private ManiXButton.XButton xButton6;
     }
 }
 
--- 193_User_Interface/193_User_Interface/Form1.cs
+++ 193_User_Interface/193_User_Interface/Form1.cs
@@ -37,14 +37,25 @@ namespace _193_User_Interface
 
         private void SerialPort()
         {
-            port = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+            port = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One);
 
             try
             {
                 port.Open();
 
-                xButton1.Text = "Connected";
-                xButton1.Theme = ManiXButton.Theme.MSOffice2010_Green;
+               // xButton1.Text = "Connected";
+               // xButton1.Theme = ManiXButton.Theme.MSOffice2010_Green;
+
+                /*if (xButton1.Text == "Not Connected")
+                {
+
+                    xButton1.Text = "Connected";
+                    xButton1.Theme = ManiXButton.Theme.MSOffice2010_RED;
+                    port.Close();
+                    //textBox1.Clear();
+
+
+                }*/
 
                 SerialPortDataReceived(port);
                 Read();
@@ -53,33 +64,128 @@ namespace _193_User_Interface
             {
                 port.Close();
                 MessageBox.Show("Connection could not be established. Please press OK and try again.");
-                Application.Exit();
-                Close();
+                //Application.Exit();
+                //Close();
             }
 
          }
 
         private void Read()
         {
-            string data = port.ReadTo(".");
-            richTextBox1.Text = data;
+            long Inlatitude;
+            long Inlongitude;
+            double latitude;
+            double longitude;
+            int Heart_Rate;
+            string Date_Time;
 
-        }
+           
+                string data = port.ReadTo("]");
 
-        public void SerialPortDataReceived(SerialPort read) // Creates buffer to read data 
-        {    
-            byte[] buf;
-            buf = new byte[read.BytesToRead];
-            read.Read(buf, 0, buf.Length);
-            string result = Encoding.UTF8.GetString(buf);    
-        }
+                string[] words = data.Split(',');
+
+                if (words[0] == null && words[1] == null)
+                {
+                    latitude = 0;
+                    longitude = 0;
+                   // Location_ShowAll_Tab.Text = ("Location set to default");
+                }
+                else if (words[0] != null && words[1] != null)
+                {
+                    try
+                    {
+                        Inlatitude = Int64.Parse(words[0]);
+                        latitude = (Inlatitude / 1000000.0);
+                        Inlongitude = Int64.Parse(words[1]);
+                        longitude = (Inlongitude / 1000000.0);
+                        textBox1.Text = ("" + latitude);
+                        textBox2.Text = ("" + longitude);
+                    //Location_ShowAll_Tab.Text = ("Lat:" + latitude + " Lon:" + longitude);
+                    map_start(latitude, longitude);
+                    }
+                    catch (FormatException)
+                    {
+                        // Location_ShowAll_Tab.Text = ("Format Issue!!!!");
+                        textBox1.Text = ("Format Issue!!!!");
+                        textBox2.Text = ("Format Issue!!!!");
+                    }
+                }
+                else if (words[0] != null && words[1] == null)
+                {
+                    latitude = 0;
+                    longitude = 0;
+                     // Location_ShowAll_Tab.Text = ("Location set to default");
+                     map_start(latitude, longitude);
+                }
+                else if (words[0] == null && words[1] != null)
+                {
+                    latitude = 0;
+                    longitude = 0;
+                     // Location_ShowAll_Tab.Text = ("Location set to default");
+                      map_start(latitude, longitude);
+                }
+                else
+                {
+                    latitude = 0;
+                    longitude = 0;
+                     //Location_ShowAll_Tab.Text = ("Location set to default");
+                      map_start(latitude, longitude);
+                }
+                if (words[2] == null)
+                {
+                    Heart_Rate = 0;
+                    textBox3.Text = ("Set to default");
+                   // Heart_Rate_ShowAll_Tab.Text = ("Set to default");
+                }
+                else
+                {
+                    try
+                    {
+                        Heart_Rate = Convert.ToInt32(words[2]);    // Convert String into int
+                        textBox3.Text = ("" + Heart_Rate);
+                       // Heart_Rate_ShowAll_Tab.Text = ("" + Heart_Rate);
+                    }
+                    catch (FormatException)
+                    {
+                        textBox3.Text = ("Format Error");
+                        //Heart_Rate_ShowAll_Tab.Text = ("Format Issue!!!!");
+                    }
+                }
+                if (words[3] == null)
+                {
+                    //Date_Time = ("Not available");
+                    textBox3.Text = ("Not available");
+                    //Date_Time_ShowAll_Tab.Text = ("Not available");
+                }
+                else
+                {
+                    try
+                    {
+                        Date_Time = words[3];                  //  Keeps string as a string
+                        textBox4.Text = ("" + Date_Time);
+                       // Date_Time_ShowAll_Tab.Text = ("" + Date_Time);
+                    }
+                    catch (FormatException)
+                    {
+                        textBox4.Text = ("Format Error");
+                      //  Date_Time_ShowAll_Tab.Text = ("Format Issue!!!!");
+                    }
+                }
+            }
+
+       // }
 
         private void gmap_Load_1(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void map_start(double x, double y)
         {
             // Initialize map:
             gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
-            gmap.Position = new PointLatLng(38.5602, -121.4241);
+            gmap.Position = new PointLatLng(x,y);
             gmap.Zoom = 15;
 
             gmap.ShowCenter = false; //don't display red cross in center of map
@@ -88,17 +194,39 @@ namespace _193_User_Interface
 
             //gmap.Overlays.Clear();
             GMapOverlay markersOverlay = new GMapOverlay("markers");
-            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(38.5602, -121.4241), GMarkerGoogleType.red_pushpin);
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(x,y), GMarkerGoogleType.red_pushpin);
             markersOverlay.Markers.Add(marker);
             gmap.Overlays.Add(markersOverlay);
+
+
+        }
+
+        public void SerialPortDataReceived(SerialPort read) // Creates buffer to read data 
+        {
+            byte[] buf;
+            buf = new byte[read.BytesToRead];
+            read.Read(buf, 0, buf.Length);
+            string result = Encoding.UTF8.GetString(buf);
         }
 
         private void xButton1_Click_1(object sender, EventArgs e)
         {
 
             SerialPort();
-            Read();
+            xButton1.Text = "Connected";
+            xButton1.Theme = ManiXButton.Theme.MSOffice2010_Green;
+            //Read();
 
+            /*if (xButton1.Text  == "Not Connected")
+            {
+                
+                xButton1.Text = "Connected";
+                xButton1.Theme = ManiXButton.Theme.MSOffice2010_RED;
+                port.Close();
+                //textBox1.Clear();
+
+
+            }*/
             /*  if (xButton1.Text == "Not Connected")
             {
                 xButton1.Text = "Connected";
@@ -113,12 +241,64 @@ namespace _193_User_Interface
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            while (true)
-            {
+            
                 Read();
-            }             
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+                Read();
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+                Read();
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+                Read();
+            
+        }
+
+        private void xButton2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void xButton3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void xButton4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void xButton5_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void xButton6_Click(object sender, EventArgs e)
+        {
+            port.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            Close();
+
         }
     }
 }
